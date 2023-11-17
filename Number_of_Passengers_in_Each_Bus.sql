8c7a398e5e5aaa388d129d25ce3ff74471d3c9b4
Nikhil Bhosale <64499517+Nikhil12121@users.noreply.github.com>
2023-11-16 19:07:38 -0600

    Number_of_Passengers_in_Each_Bus.sql

---
+++ Number_of_Passengers_in_Each_Bus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+WITH CTE AS ( SELECT p.passenger_id, min(b.arrival_time) AS arrival_time FROM Passengers p INNER JOIN Buses b ON p.arrival_tiime <= b.arrival_time GROUP BY p.passenger_id
+)
+
+SELECT b.bus_id,COUNT(CTE.arrival_time) AS passengers_cnt
+FROM Buses b
+LEFT JOIN CTE ON b.arrival_time = CTE.arrival_time
+GROUP BY b.bus_id
+ORDER BY b.bus_id;
