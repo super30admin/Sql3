0bd4269c0be9807d93f17634b818db0919a92671
harshita patil <harshita.patil1992@gmail.com>
2023-11-22 14:14:58 -0800

    Update User Activity .sql

--- User Activity.sql
+++ User Activity.sql
@@ -5,6 +5,6 @@
 Select a1.activity_date as day, count(distinct a1.user_id) as active_users
 FROM 
       Activity a1
-where DATEDIFF('2019-07-27',a1.activity_date)  30
+where DATEDIFF('2019-07-27',a1.activity_date) < 30
       and a1.activity_date = '2019-07-27'
 group by a1.activity_date
